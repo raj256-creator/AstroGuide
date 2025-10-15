e91fbfcea345a0f78c7996d227a4f28309660df0
raj256-creator <moviex22557@gmail.com>
2025-10-15 12:56:27 +0530

    Fix Babel configuration to transpile Expo modules

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,16 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            'expo-modules-core': 'expo-modules-core/build',
+            'expo-web-browser': 'expo-web-browser/build'
+          }
+        }
+      ]
+    ]
   };
 };
